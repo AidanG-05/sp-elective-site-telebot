de44fbace9bdb4f8d7b1237760603a7dd6ef1445
Theyan <rstheyanashvaran17.22@ichat.sp.edu.sg>
2025-06-19 19:51:21 +0800

    Fix

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ def telegram_webhook():
     return jsonify({"status": "OK"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
